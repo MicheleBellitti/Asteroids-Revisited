13fe0f3efe8291e3e1b67f9153390e0395b8407d
MicheleBellitti <michelebellitti272@gmail.com>
2021-11-10 16:37:49 +0100

    coin added

---
+++ src/src/main/java/Coin.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class Coin extends GameObject{
+    private int scorePoints;
+    public Coin(float x, float y, float velx, float vely, ID id) {
+        super(x, y, velx, vely, id);
+        width=15;
+        height=15;
+        scorePoints=150;
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawOval((int)x,(int)y,width+1,height+1);
+        g.fillOval((int)x,(int)y,width,height);
+    }
+}

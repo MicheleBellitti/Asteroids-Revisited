e3c5789d327c32a59e7dc20716b557cfc00d6bbd
MicheleBellitti <michelebellitti272@gmail.com>
2021-11-15 16:48:52 +0100

    ML

--- src/src/main/java/MyMouseListener.java
+++ src/src/main/java/MyMouseListener.java
@@ -21,14 +21,14 @@ public class MyMouseListener extends MouseAdapter {
         int mx,my;
         mx=e.getX();
         my=e.getY();
-        if(tmp!=null){
+        /*if(tmp!=null){
             GameObject tmpBullet=new Bullet(tmp.getX() + 16, tmp.getY() + 16, 0,0 , ID.Bullet);
             handler.addGameObject(tmpBullet);
             float angle=(float) Math.atan2(my-tmp.getY()-16,mx-tmp.getX()-16);
             int bullVel=10;
             tmpBullet.setVelX((float) (bullVel*Math.cos(angle)));
             tmpBullet.setVelY((float) (bullVel*Math.sin(angle)));
-        }
+        }*/
 
 
         if(StartMenu.on==true) {

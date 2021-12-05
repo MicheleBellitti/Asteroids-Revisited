d5be336ec6ee3546c672081250519fc2333fc5e2
MicheleBellitti <michelebellitti272@gmail.com>
2021-12-05 23:39:25 +0100

    enemy e bullet sprites update.

--- src/src/main/java/Game.java
+++ src/src/main/java/Game.java
@@ -88,7 +88,7 @@ public class Game extends Canvas implements Runnable {
         coinhit=new GameSound(".\\src\\src\\main\\resources\\Coinraccolto.wav");
         playerhit=new GameSound(".\\src\\src\\main\\resources\\Playercolpito.wav");
         play= new GameSound(".\\src\\src\\main\\resources\\Street Fighter III 3rd Strike-The Theme of Q.wav");
-        this.handler.addGameObject(new Player(350.0F, (float) (HEIGHT - 75), 0.0F, 0.0F, ID.Player));
+        this.handler.addGameObject(new Player(350.0F, (float) (HEIGHT - 125), 0.0F, 0.0F, ID.Player));
         for (int i = 0; i < 3; i++) {
             this.handler.addGameObject(new Enemy((float)r1.nextInt(WIDTH), 0.0F, 2*J, 2.0F, ID.Enemy));
             this.J *= -1;
--- src/src/main/java/Player.java
+++ src/src/main/java/Player.java
@@ -10,7 +10,7 @@ import javax.swing.*;
 public class Player extends GameObject{
    private static float HEIGHT=64;
    public  SpriteSheet playerSprite=new SpriteSheet();
-
+    BufferedImage transparentImg;
 
     public Player(float x, float y,float vx,float vy, ID id) {
         super(x,y,vx,vy,id);
@@ -30,11 +30,11 @@ public class Player extends GameObject{
 
     @Override
     public void render(Graphics g) {
-
-        BufferedImage transparentImg=playerSprite.getImage("./src/src/main/resources/asteroids-arcade.png" ).getSubimage(0,64,64,64);
+        if(velY<=0)
+        transparentImg=playerSprite.getImage("./src/src/main/resources/asteroids-arcade.png" ).getSubimage(0,64,64,64);
+        else  transparentImg=playerSprite.getImage("./src/src/main/resources/asteroids-arcade-upsidedown.png" ).getSubimage(256-64,128,64,64);
         if (Game.color==Color.pink) {
-            g.setColor(Color.black.darker());
-            g.fill3DRect((int) x, (int) y, 32, 32, true);
+            g.drawImage(transparentImg,(int)x,(int)y,null);
         }
         if (Game.color==Color.black) {
            // g.setColor(Color.white.darker());
@@ -44,16 +44,13 @@ public class Player extends GameObject{
 
         }
         if (Game.color==Color.white) {
-            g.setColor(Color.black.darker());
-            g.fill3DRect((int) x, (int) y, 32, 32, true);
+            g.drawImage(transparentImg,(int)x,(int)y,null);
         }
         if (Game.color==Color.blue) {
-            g.setColor(Color.white.darker());
-            g.fill3DRect((int) x, (int) y, 32, 32, true);
+            g.drawImage(transparentImg,(int)x,(int)y,null);
         }
         if (Game.color==Color.red) {
-            g.setColor(Color.white.darker());
-            g.fill3DRect((int) x, (int) y, 32, 32, true);
+            g.drawImage(transparentImg,(int)x,(int)y,null);
         }
 
     }

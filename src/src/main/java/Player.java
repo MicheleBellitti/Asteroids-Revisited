ac04c3d70f1165da79a51495a6f6a229376a5c7e
MicheleBellitti <michelebellitti272@gmail.com>
2021-12-05 18:27:33 +0100

    player

--- src/src/main/java/Player.java
+++ src/src/main/java/Player.java
@@ -9,30 +9,15 @@ import javax.swing.*;
 
 public class Player extends GameObject{
    private static float HEIGHT=32;
-    public static BufferedImage img;
+   public static SpriteSheet playerSprite=new SpriteSheet();
 
-    {
-        try {
-            img = ImageIO.read(new File("./src/src/main/resources/test3.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public Player(float x, float y,float vx,float vy, ID id) {
         super(x,y,vx,vy,id);
-    }
-    ImageFilter filter = new RGBImageFilter() {
-        int transparentColor = Color.white.getRGB() | 0xFF000000;
+        Player.playerSprite.getImage("./src/src/main/resources/test3.png");
 
-        public final int filterRGB(int x, int y, int rgb) {
-            if ((rgb | 0xFF000000) == transparentColor) {
-                return 0x00FFFFFF & rgb;
-            } else {
-                return rgb;
-            }
-        }
-    };
+    }
+
 
 
     @Override
@@ -42,25 +27,11 @@ public class Player extends GameObject{
         x=Game.clamp((int)x,0,Game.WIDTH-30);
         y=Game.clamp((int)y,0,Game.HEIGHT-67);
     }
-    public static  BufferedImage makeTransparentImage(BufferedImage br) {
-        for (int i = 0; i < br.getHeight(); i++) {
-            for (int j = 0; j < br.getWidth(); j++) {
-                Color c = new Color(br.getRGB(j, i));
-                int r = c.getRed();
-                int b = c.getBlue();
-                int g = c.getGreen();
-                if ((r == 255 && b == 255 && g == 255)) {
-                    System.out.println("r g b " + r + g + b);
-                    br.setRGB(j, i, 0xFF000000);
-                }
-            }
-        }
-        return br;
-    }
+
     @Override
     public void render(Graphics g) {
 
-        BufferedImage transparentImg=makeTransparentImage(img.getSubimage(0,0,img.getWidth()/6,32));
+        BufferedImage transparentImg=SpriteSheet.makeTransparentImage(playerSprite.getSubimage(0,0,playerSprite.getWidth()/6,32));
         if (Game.color==Color.pink) {
             g.setColor(Color.black.darker());
             g.fill3DRect((int) x, (int) y, 32, 32, true);
